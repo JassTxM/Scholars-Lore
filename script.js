cb896ce534acbdbe7f3f40e7922d130951962b10
Jaskirat Singh <jasstxm@gmail.com>
2025-04-21 22:12:16 +0530

    Revert "[JS]" due to wrong commit message
    This reverts commit 584ee0018a6ae5328610736e6f586d8a017452a6.

--- script.js
+++
@@ -1,7 +0,0 @@
-const billForm = document.querySelector('.bill-form');
- if (billForm) {
-        billForm.addEventListener('submit', function(k) {
-            k.preventDefault(); // Prevent form submission
-            alert('Payment processed successfully! Redirecting to confirmation page...');
-        });
-}
