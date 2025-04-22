1e948eb1c5c331722d078f6d2e7abb54714df098
Jaskirat Singh <jasstxm@gmail.com>
2025-04-23 00:01:49 +0530

    [JS] | Create script for bill-form in script.js

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ if (contactForm) {
             alert('Your request has been sent. Please wait till our team contacts you!');
         });
  }
+
+ const billForm = document.querySelector('.bill-form');
+ if (billForm) {
+        billForm.addEventListener('submit', function(k) {
+            k.preventDefault(); // Prevent form submission
+            alert('Payment processed successfully! Redirecting to confirmation page...');
+        });
+}
